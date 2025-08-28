1bd37e2cfe0f5c90df5d1f1dc3acd9a98ad8d189
Hayden <hayden.t.durham@gmail.com>
2025-08-28 13:41:30 -0500

    Added InMemoryUserDetailsManager()

--- src/main/java/rent/properly/properly/Security/SecurityConfig.java
+++ src/main/java/rent/properly/properly/Security/SecurityConfig.java
@@ -3,9 +3,14 @@ package rent.properly.properly.Security;
 import org.springframework.beans.factory.config.CustomEditorConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,8 +22,29 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                .anyRequest().authenticated())
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        UserDetails landlord = User.builder()
+                .username("landlord")
+                .password("password")
+                .roles("LANDLORD")
+                .build();
+        UserDetails tenant = User.builder()
+                .username("tenant")
+                .password("password")
+                .roles("TENANT")
+                .build();
+        return new InMemoryUserDetailsManager(admin, landlord, tenant);
     }
 }
