c95849401a648391a7d291df385f3468d3138543
Hayden <hayden.t.durham@gmail.com>
2025-09-02 20:43:17 -0500

    Got rid of secrets

--- src/main/java/rent/properly/security/JWTGenerator.java
+++ src/main/java/rent/properly/security/JWTGenerator.java
@@ -5,23 +5,34 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
+@EnableConfigurationProperties(JwtProperties.class)
 public class JWTGenerator {
+    private final JwtProperties jwt;
+    private final SecretKey key;
 
-    private byte[] keyBytes = Decoders.BASE64.decode(SecurityConstants.JWT_SECRET);
-    private SecretKey key = Keys.hmacShaKeyFor(keyBytes);
+    public JWTGenerator(JwtProperties jwt) {
+        this.jwt = jwt;
+        String secret = jwt.getSecret();
+        if (!StringUtils.hasText(secret)) {
+            throw new IllegalArgumentException("Secret must not be empty");
+        }
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
 
     public String generateToken(Authentication authentication) {
         String username = authentication.getName();
         Date currentDate = new Date();
-        Date expireDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+        Date expireDate = new Date(currentDate.getTime() + jwt.getExpirationTime());
 
         String token = Jwts.builder()
                 .setSubject(username)
